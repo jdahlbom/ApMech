397c6444e8d72522a8736c08bddebec9ccfeecaf
Jonni Lehtiranta <jonni@vinyamar.lehtiranta.net>
2008-12-21 15:12:47 +0200

    Made server notify clients of disconnects, other minor fixes and cleaning

--- sample1_jonni/src/functions.h
+++ sample1_jonni/src/functions.h
@@ -11,4 +11,7 @@
 using namespace std;
 
 string uint2ipv4(unsigned int ip);  // Convert a uint32 to a string with the corresponding IP address
-void hexprint(enet_uint8 *buffer, int length);
+
+void hexprint(enet_uint8 *buffer, int length); // Cout a random buffer and maybe understand what's in it!
+
+template <class T> inline std::string to_string (const T &t); // generic to_string() for any pipeable thing
--- sample1_jonni/src/main.cpp
+++ sample1_jonni/src/main.cpp
@@ -17,60 +17,64 @@ using namespace std;
 
 int main ( int argc, char** argv )
 {
-  SDL_Surface *screen;
-  SDL_Rect dstrect;
+    SDL_Surface *screen;
+    SDL_Rect dstrect;
 
-  NetData *netdata;
-  int client = 0;
-  srand(time(NULL));
+    NetData *netdata;
+    srand(time(NULL));
 
-  if (argc == 2) {
-    cout << "Created a server."<<endl;
-    netdata = new NetData(NetData::SERVER);
-  } else {
-    netdata = new NetData(NetData::CLIENT); client = 1;
-    netdata->connect("192.168.2.3", 5074);
-    netdata->me.nick = "Test";
-    netdata->me.x = -1; netdata->me.y = 2;
-    netdata->me.color = 0x00FFFFFF;
-    netdata->me.changed = true; // Mark all info for transmission
+    if (argc == 2) {
+        cout << "Created a server."<<endl;
+        netdata = new NetData(NetData::SERVER);
 
-    if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )    // initialize SDL video
-    {
-        printf( "Unable to init SDL: %s\n", SDL_GetError() );
-        return 1;
-    }
-    atexit(SDL_Quit);// make sure SDL cleans up before exit
-
-    // create a new window
-    screen = SDL_SetVideoMode(640, 480, 24, SDL_HWSURFACE|SDL_DOUBLEBUF);
-    if ( !screen )
-    {
-        printf("Unable to set 640x480 video: %s\n", SDL_GetError());
-        return 1;
-    }
-    SDL_EnableKeyRepeat(10,10);
-
-    dstrect.x = (screen->w)/2; dstrect.y = (screen->h)/2;
-    dstrect.w = 10; dstrect.h = 10;
-  }
+        while (1) {             // Server main loop
+            netdata->service();
+            SDL_Delay(5);
+        }
 
 
-    // program main loop
-    bool done = false;
-    while (!done)
-    {
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
+    } else {                  // ************************ Client code
+
+        map<int,NetUser>::iterator p = netdata->users.begin();
+
+        netdata = new NetData(NetData::CLIENT);
+        netdata->connect("192.168.2.3", 5074);
+        netdata->me.nick = "Test";
+        netdata->me.changed = true; // Mark all info for transmission
+
+        if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )    // initialize SDL video
         {
-            // check for messages
-            switch (event.type)
+            printf( "Unable to init SDL: %s\n", SDL_GetError() );
+            return 1;
+        }
+        atexit(SDL_Quit);// make sure SDL cleans up before exit
+
+        // create a new window
+        screen = SDL_SetVideoMode(640, 480, 24, SDL_HWSURFACE|SDL_DOUBLEBUF);
+        if ( !screen )
+        {
+            printf("Unable to set 640x480 video: %s\n", SDL_GetError());
+            return 1;
+        }
+        SDL_EnableKeyRepeat(10,10);
+
+        dstrect.x = (screen->w)/2; dstrect.y = (screen->h)/2;
+        dstrect.w = 10; dstrect.h = 10;
+
+        // program main loop
+        bool done = false;
+        while (!done)
+        {
+            SDL_Event event;
+            while (SDL_PollEvent(&event))
             {
-            case SDL_QUIT:
-                done = true;
-                break;
-            case SDL_KEYDOWN:
+                // check for messages
+                switch (event.type)
                 {
+                case SDL_QUIT:
+                    done = true;
+                    break;
+                case SDL_KEYDOWN:
                     switch (event.key.keysym.sym)
                     {
                      case SDLK_ESCAPE:
@@ -94,36 +98,33 @@ int main ( int argc, char** argv )
                         netdata->me.x++; netdata->me.changed = true;
                         break;
                      default:
-                        cout <<"P!";
                         break;
-                    }
+                    } // inner switch
 
-//                        SDL_MapRGB(screen->format, 200,100,0));
                     break;
                 }
-            } // end switch
-        } // end of message processing
-        if (client) {
-            map<int,NetUser>::iterator p = netdata->users.begin();
+            } // end of message processing
 
             SDL_FillRect(screen, 0, SDL_MapRGB(screen->format, 0, 0, 0)); // clear screen
+
+            p = netdata->users.begin();
             while (p != netdata->users.end()) {
                 dstrect.x = (screen->w)/2 + p->second.x;
                 dstrect.y = (screen->h)/2 + p->second.y;
                 SDL_FillRect(screen, &dstrect, p->second.color);
                 p++;
             }
+
             SDL_Flip(screen);        // finally, update the screen :)
-           // cout << netdata->users.size() << " connections." << endl;
-        }
 
-        netdata->service();
-        SDL_Delay(5);
-    } // end main loop
+            netdata->service();
+            SDL_Delay(5);
+        } // end main loop
 
 
-  // all is well ;)
-  delete netdata;
-  printf("Exited cleanly\n");
-  return 0;
+        // all is well ;)
+        delete netdata;
+        printf("Exited cleanly\n");
+        return 0;
+    }
 }
