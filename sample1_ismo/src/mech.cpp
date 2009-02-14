4faeb47e7873b142300927b1f8a3e3f3576d8dd3
Ismo Puustinen <ismo@iki.fi>
2009-02-14 13:24:19 +0200

    Changed serialize to use unsigned char array instead of uint8_t.

--- sample1_ismo/src/mech.cpp
+++ sample1_ismo/src/mech.cpp
@@ -6,6 +6,6 @@ Mech::Mech(int id): GameObject(id) {
 Mech::~Mech() {
 }
 
-int Mech::serialize(uint8_t buffer[], int start, int buflength) {
+int Mech::serialize(unsigned char buffer[], int start, int buflength) {
 	return 0;
 }
