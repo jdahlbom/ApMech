8e599b1ff0dc6e40137cc43d33e2cb1bb689039e
Jukka Dahlbom <jukkad@Serenity.(none)>
2009-08-02 22:18:02 +0300

    Added a general purpose constants-file.

---
+++ src/constants.h
@@ -0,0 +1,13 @@
+#ifndef AP_CONSTANTS_H
+#define AP_CONSTANTS_H
+
+#include "types.h"
+
+namespace ap {
+    static const uint8 OBJECT_TYPE_UNDEFINED = 0;
+    static const uint8 OBJECT_TYPE_MECH = 1;
+    static const uint8 OBJECT_TYPE_PROJECTILE = 2;
+}
+
+
+#endif
