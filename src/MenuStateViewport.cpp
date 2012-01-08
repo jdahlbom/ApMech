7579bc81abd6f41dc870bab8ffc425e20b08784a
Jonni Lehtiranta <jonni@lehtiranta.net>
2012-01-08 19:06:46 +0200

    Restored camera creating and destroying in MenuStateViewport.cpp. This seems to have no ill effect.

--- src/MenuStateViewport.cpp
+++ src/MenuStateViewport.cpp
@@ -17,24 +17,27 @@ namespace ap {
     assert(pRoot);
 
     mCamera = sceneMgr->createCamera( "MenuCamera" );
-    
+    // JL 8.1.2012: For some reason, creating and deleting viewports makes problems.
+    // So we don't do that. Unless someone figures out what's wrong.
+    // Downside: Now mouse pointer leaves trails in menus.
+/*
     // REQUIRES for the RenderTarget named "ApMech" to exist..
     Ogre::RenderTarget *renderWindow = pRoot->getRenderTarget("ApMech");
     assert(renderWindow);
-    
+
     Ogre::ColourValue bgColor(0.0f, 0.0f, 0.0f);
     mMenuViewport = renderWindow->addViewport(mCamera);
     mMenuViewport->setBackgroundColour(bgColor);
-  }
+*/  }
   MenuStateViewport::~MenuStateViewport()
   {
     pSceneMgr->destroyCamera(mCamera);
-
+/*
     Ogre::Root *pRoot = Ogre::Root::getSingletonPtr();
     assert(pRoot);
-    pRoot->getRenderTarget("ApMech")->removeAllViewports();    
+    pRoot->getRenderTarget("ApMech")->removeAllViewports();
 
     // Ogre::Root::getSingletonPtr()->getRenderTarget("ApMech")->removeViewport(mMenuViewport->getZOrder());
-  }
-  
+  */}
+
 } // namespace ap
